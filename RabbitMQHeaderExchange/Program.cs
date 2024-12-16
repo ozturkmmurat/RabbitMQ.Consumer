cfab236e383046efb6905038e0bea97b3a0f2cf5
ozturkmmurat <murat.oztrkk01@gmail.com>
2024-12-16 19:50:55 +0300

    Header Exchange uygulandı.

---
+++ RabbitMQHeaderExchange/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://mbnjciig:pyWEy4hPFEOr0FY7lEe1SmJos7Emj7MB@toad.rmq.cloudamqp.com/mbnjciig");
+
+//Baglanti Aktifleştirme ve Kanal Açma,
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange:"header-exchange-example",
+    type: ExchangeType.Headers);
+
+Console.Write("Lütfen header value'sunu giriniz : ");
+string value = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue:queueName,
+    exchange: "header-exchange-example",
+    routingKey: string.Empty,
+    new Dictionary<string, object>
+    {
+        ["x-match"] = "any",
+        ["no"] = value
+    });
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
